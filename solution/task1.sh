21a058f19dbaea798cbb260b2817c532781e309b
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-09-10 19:16:59 +0700

    fix the paths

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,10 +1,8 @@
 ROOT_DIR="$(dirname $(dirname $(realpath $0)))"
 
-mkdir tmp
+mkdir "$ROOT_DIR/tmp"
 
-tar -xf "$ROOT_DIR/data/archive-part1.tar" -C tmp
-unzip "$ROOT_DIR/data/archive-part2.zip" -d tmp
+tar -xf "$ROOT_DIR/data/archive-part1.tar" -C "$ROOT_DIR/tmp"
+unzip "$ROOT_DIR/data/archive-part2.zip" -d "$ROOT_DIR/tmp"
 
-tar -czf "$ROOT_DIR/data/archive-combined.tar.gz" -C tmp .
-
-rm -r tmp
+tar -czf "$ROOT_DIR/data/archive-combined.tar.gz" -C "$ROOT_DIR/tmp" "$ROOT_DIR/tmp"
