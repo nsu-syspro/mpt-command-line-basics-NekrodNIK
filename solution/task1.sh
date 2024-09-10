65bd9f9e4315192a901e29ab3aa188fbecd5115f
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-09-10 19:25:30 +0700

    make paths relative

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,10 +1,6 @@
-ROOT_DIR="$(dirname $(dirname $(realpath $0)))"
+mkdir "./tmp"
 
-mkdir "$ROOT_DIR/tmp"
+tar -xf "./data/archive-part1.tar" -C "./tmp"
+unzip "./data/archive-part2.zip" -d "./tmp"
 
-tar -xf "$ROOT_DIR/data/archive-part1.tar" -C "$ROOT_DIR/tmp"
-unzip "$ROOT_DIR/data/archive-part2.zip" -d "$ROOT_DIR/tmp"
-
-tar -czf "$ROOT_DIR/data/archive-combined.tar.gz" -C "$ROOT_DIR/tmp" "$ROOT_DIR/tmp"
-
-echo $(tree ./)
+tar -czf "./data/archive-combined.tar.gz" -C "./tmp" "./tmp"`
