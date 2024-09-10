5fe272a8a918d01f9ed0b92e56bfd61817a47d86
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-09-10 20:09:05 +0700

    fix typos

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,5 +1,5 @@
-FOO_PATH=$(realpath $(find -name foo.txt))
-BAR_PATH=$(realpath $(find -name bar.txt))
-BAZ_PATH=$(realpath $(find -name baz.txt))
+FOO_PATH=$(find -name foo.txt)
+BAR_PATH=$(find -name bar.txt)
+BAZ_PATH=$(find -name baz.txt)
 
 cat $FOO_PATH $BAR_PATH $BAZ_PATH
