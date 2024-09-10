8b646da79e0db8efb20218ef1f6a0205408a43ad
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-09-10 19:21:07 +0700

    debug classroom

--- solution/task1.sh
+++ solution/task1.sh
@@ -6,3 +6,5 @@ tar -xf "$ROOT_DIR/data/archive-part1.tar" -C "$ROOT_DIR/tmp"
 unzip "$ROOT_DIR/data/archive-part2.zip" -d "$ROOT_DIR/tmp"
 
 tar -czf "$ROOT_DIR/data/archive-combined.tar.gz" -C "$ROOT_DIR/tmp" "$ROOT_DIR/tmp"
+
+echo tree ./
