4ed2f8fd36f6405abbab1aed3e8eab73d3f16ac6
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2024-09-10 20:13:52 +0700

    set find -type as f

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,5 +1,5 @@
-FOO_PATH=$(find -name foo.txt)
-BAR_PATH=$(find -name bar.txt)
-BAZ_PATH=$(find -name baz.txt)
+FOO_PATH=$(find -type f -name foo.txt)
+BAR_PATH=$(find -type f -name bar.txt)
+BAZ_PATH=$(find -type f -name baz.txt)
 
 cat $FOO_PATH $BAR_PATH $BAZ_PATH
